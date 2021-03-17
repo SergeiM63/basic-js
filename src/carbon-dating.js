e4c3a67158b18efda92e0b0dd7e513a40eb9a415
Sergei M <61053189+SergeiM63@users.noreply.github.com>
2021-03-17 22:09:41 +0400

    fix: fix code

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,15 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY = 15;
-const HALF_LIFE_PERIOD = 5730; 
+const MODERN_ACTIVITY= 15;
+const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
   if (
-    typeof sampleActivity != String ||
-    typeof sampleActivity == null ||
-    typeof sampleActivity == undefined
-  ) return false;
-  
-  let t = (parseInt(sampleActivity) * HALF_LIFE_PERIOD) / MODERN_ACTIVITY;
-  return Math.ceil(t);
+    typeof sampleActivity !== "string" ||
+    /[A-Za-z]/.test(sampleActivity) ||
+    sampleActivity <= 0 ||
+    sampleActivity > MODERN_ACTIVITY
+  )
+    return false;
+
+  let rate = Math.LN2.toFixed(3) / HALF_LIFE_PERIOD;
+  let years = Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / rate;
+  return Math.ceil(years);
 };
