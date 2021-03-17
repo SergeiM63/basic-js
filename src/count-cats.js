64b20a2dc1b1d8364a03de122ea6626b135c7018
Sergei M <61053189+SergeiM63@users.noreply.github.com>
2021-03-17 21:43:29 +0400

    refactor: refactor code

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
-  let countCats = 0;
-  matrix.map(item => {
-    if(item.includes('^^')) countCats++;
-  });
-  return countCats;
+  return matrix.flat().filter(cat => cat === '^^').length;
 };
